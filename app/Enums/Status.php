09307d3904ee4876dc48ce7a416a813885206fb9
mustafah15 <mustafa.hussain93@gmail.com>
2017-02-16 11:12:08 +0200

    initail commit

---
+++ app/Enums/Status.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 2/16/17
+ * Time: 9:23 AM
+ */
+
+namespace App\Enums;
+
+
+abstract class Status
+{
+    const  InProcess = 0;
+    const Done = 1;
+    const Complete = 2;
+}
---
+++ app/Task.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 2/16/17
+ * Time: 9:04 AM
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Task extends Model
+{
+
+    protected $table = 'tasks';
+
+}
